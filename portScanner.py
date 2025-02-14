73d60b0d0d384c106b73fff0cbbd95383867c57d
rojounooo <rojounooo@gmail.com>
2025-02-14 11:36:55 +0000

    Updated script

--- portScanner.py
+++ portScanner.py
@@ -5,16 +5,18 @@ import sys
 # Get target from user 
 if len(sys.argv) > 1:
     target = sys.argv[1]
-    file = f"{target}ScanResults"
 else: 
     print("No target provided")
     sys.exit(1)  # Exit if no target is provided
+
+# Create the filename
+file = f"ScanResults.txt"
 
 # Create an empty list for addresses 
 addresses = []
 
 # -sn scan for live host discovery
-os.system(f"nmap -sN {target} -oN {file}")  
+os.system(f"nmap -sn {target} -oN {file}")  
 
 # RegEx to filter for live hosts
 with open(file, "r") as resultsFile:
@@ -22,6 +24,11 @@ with open(file, "r") as resultsFile:
     addresses = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", lines)  # Extract IPs
 
 # Write filtered addresses to a file
-with open("filteredResults.txt", "w") as fResults:
+with open("filteredAddresses.txt", "w") as fResults:
     for address in addresses:
+        fResults.write("Results From Host Discovery: \n")
         fResults.write(address + "\n")
+
+
+# Perform a stealth scan 
+os.system(f"nmap -sS -p- -iL {fResults}")
